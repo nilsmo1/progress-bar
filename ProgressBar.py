9f6c59e6288df253d9ca259e9ae065befdca3491
Nils Mo <nilsmo1@hotmail.com>
2022-05-31 02:39:26 +0200

    Display task N/A if description not given

--- ProgressBar.py
+++ ProgressBar.py
@@ -17,6 +17,6 @@ class ProgressBar:
     def display(self, description: Optional[str]=None) -> None:
         progress = int(100 * self.current / float(self.total))
         remaining = 100 - progress
-        output = lambda long, done, color: (f"\r|{color + Style.BRIGHT + '█' * floor(progress/10 if not long else progress) + Style.RESET_ALL}{color + Style.DIM + '█' * ceil(remaining/10 if not long else remaining) + Fore.RESET + Style.RESET_ALL}|{progress}% ({color}{self.current}{Fore.RESET}/{self.total}), Task: {description}")
+        output = lambda long, done, color: (f"\r|{color + Style.BRIGHT + '█' * floor(progress/10 if not long else progress) + Style.RESET_ALL}{color + Style.DIM + '█' * ceil(remaining/10 if not long else remaining) + Fore.RESET + Style.RESET_ALL}|{progress}% ({color}{self.current}{Fore.RESET}/{self.total}), Task: {description or 'N/A'}")
         done = self.current == self.total
         print(output(self.long, done, done*Fore.GREEN + (not done)*Fore.YELLOW) + "\n"*done, end="\r")
